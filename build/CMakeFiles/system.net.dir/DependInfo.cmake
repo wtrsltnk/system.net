fd5391c45d6d9c737d0720655e11a1c9bf6a0727
Wouter Saaltink <wouter.saaltink@gmail.com>
2018-06-07 23:52:06 +0200

    Change filename convention

---
+++ build/CMakeFiles/system.net.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Code/small-apps/system.net/src/http/HttpListener.cpp" "C:/Code/small-apps/system.net/build/CMakeFiles/system.net.dir/src/http/httplistener.cpp.obj"
+  "C:/Code/small-apps/system.net/src/http/HttpListenerContext.cpp" "C:/Code/small-apps/system.net/build/CMakeFiles/system.net.dir/src/http/httplistenercontext.cpp.obj"
+  "C:/Code/small-apps/system.net/src/http/HttpListenerException.cpp" "C:/Code/small-apps/system.net/build/CMakeFiles/system.net.dir/src/http/httplistenerexception.cpp.obj"
+  "C:/Code/small-apps/system.net/src/http/HttpListenerRequest.cpp" "C:/Code/small-apps/system.net/build/CMakeFiles/system.net.dir/src/http/httplistenerrequest.cpp.obj"
+  "C:/Code/small-apps/system.net/src/http/HttpListenerResponse.cpp" "C:/Code/small-apps/system.net/build/CMakeFiles/system.net.dir/src/http/httplistenerresponse.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
